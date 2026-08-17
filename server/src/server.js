45c281ab4e0339616bb72974c7895bb3d067c183
Thanu Venu <thanu.venu28@gmail.com>
2026-08-17 11:17:39 +0530

    fix: configure backend for render

--- server/src/server.js
+++ server/src/server.js
@@ -26,7 +26,7 @@ mongoose
     .then(() => {
         console.log("MongoDB connected");
 
-        const PORT = process.env.PORT || 5000;
+        const PORT = process.env.PORT || 10000;
 
         app.listen(PORT, () => {
             console.log(`Server running on port ${PORT}`);
